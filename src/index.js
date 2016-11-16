99cc349aa80bd1c50aeb612e41f22d249147f747
jeffersons <jeffersonsong@yahoo.com>
2016-11-15 22:17:10 -0500

    16 - 1

--- src/index.js
+++ src/index.js
@@ -49,7 +49,23 @@ const visibilityFilter = (
   }
 };
 
-const { combineReducers } = Redux;
+// const { combineReducers } = Redux;
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState;
+      },
+      {}
+    );
+  };
+};
+
 const todoApp = combineReducers({
   todos,
   visibilityFilter
