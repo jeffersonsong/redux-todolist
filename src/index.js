982e1b8ecc3daf58b9450f617f356453dfc2f8cf
jeffersons <jeffersonsong@yahoo.com>
2016-11-15 21:49:57 -0500

    12

--- src/index.js
+++ src/index.js
@@ -6,9 +6,20 @@ const todos = (state = [], action) => {
         {
           id : action.id,
           text : action.text,
-          complete : false
+          completed : false
         }
       ];
+
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id != action.id) {
+          return todo;
+        }
+
+        return Object.assign({}, todo, {
+          completed : !todo.completed
+        });
+      });
 
     default:
       return state;
@@ -27,7 +38,47 @@ const testAddToDo = () => {
     {
       id : 0,
       text : 'Lean Redux',
-      complete : false
+      completed : false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id : 0,
+      text : 'Learn Redux',
+      completed : false
+    },
+    {
+      id : 1,
+      text : 'Go shopping',
+      completed : false
+    }
+  ];
+
+  const action = {
+    type : 'TOGGLE_TODO',
+    id : 1
+  };
+
+  const stateAfter = [
+    {
+      id : 0,
+      text : 'Learn Redux',
+      completed : false
+    },
+    {
+      id : 1,
+      text : 'Go shopping',
+      completed : true
     }
   ];
 
@@ -40,4 +91,5 @@ const testAddToDo = () => {
 };
 
 testAddToDo();
+testToggleTodo();
 console.log('All tests passed');
