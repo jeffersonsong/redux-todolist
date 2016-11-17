34bac82d40bff3ca240490b87b2b789e9dbe31eb
jeffersons <jeffersonsong@yahoo.com>
2016-11-16 22:16:22 -0500

    20

--- src/index.js
+++ src/index.js
@@ -83,6 +83,41 @@ const FilterLink = ({
   );
 };
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li 
+    onClick={onClick}
+    style= {{
+      textDecoration:
+        completed ? 
+          'line-through' :
+          'none'
+    }}>
+    {text}
+  </li>
+);
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+  {
+    todos.map(todo =>
+      <Todo 
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )
+  }
+
+  </ul>
+);
+
 const getVisibleTodos = (
   todos,
   filter
@@ -129,25 +164,14 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-                onClick={() => {
-                store.dispatch({
-                  type : 'TOGGLE_TODO',
-                  id : todo.id
-                });
-              }}
-              style= {{
-                textDecoration:
-                  todo.completed ? 
-                    'line-through' :
-                    'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList 
+          todos={visibleTodos}
+          onTodoClick={id => 
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+        }/>
         <p>
           Show:
           {' '}
