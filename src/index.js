136ecdac50a3101913d1847595c2ad3045bc1352
jeffersons <jeffersonsong@yahoo.com>
2016-11-16 22:21:33 -0500

    21 - 1

--- src/index.js
+++ src/index.js
@@ -118,6 +118,25 @@ const TodoList = ({
   </ul>
 );
 
+const AddTodo = ({
+  onAddClick
+}) => {
+  let input;
+  return (
+    <div>
+      <input ref={node => {
+        input = node;
+      }} />
+      <button onClick={() => {
+        onAddClick(input.value);
+        input.value = '';
+      }}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
 const getVisibleTodos = (
   todos,
   filter
@@ -151,19 +170,15 @@ class TodoApp extends Component {
 
     return (
       <div>
-        <input ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={() => {
-          store.dispatch({
-            type : 'ADD_TODO',
-            text : this.input.value,
-            id : nextTodoId++
-          });
-          this.input.value = '';
-        }}>
-          Add Todo
-        </button>
+        <AddTodo 
+          onAddClick={text =>
+            store.dispatch({
+              type : 'ADD_TODO',
+              id : nextTodoId++,
+              text
+            })
+          }
+        />
         <TodoList 
           todos={visibleTodos}
           onTodoClick={id => 
